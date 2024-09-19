cddc5c7dc0aa4ee19ee707e16547cd2f1a758ca6
Karla <kr8940650@gmail.com>
2024-09-19 16:00:37 -0600

    Verificacion inicio

---
+++ src/app/demo/components/verificacion-auth.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VerificacionAuthRoutingModule } from './verificacion-auth-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    VerificacionAuthRoutingModule
+  ]
+})
+export class VerificacionAuthModule { }
---
+++ src/app/demo/components/verificacion/verificacionAuth/verificacion-auth/verificacion-auth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verificacion-auth',
+  standalone: true,
+  imports: [],
+  templateUrl: './verificacion-auth.component.html',
+  styleUrl: './verificacion-auth.component.scss'
+})
+export class VerificacionAuthComponent {
+
+}
