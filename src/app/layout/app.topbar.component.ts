2ec77b1429eccb6058909e827e0f044c8d2cb49f
Gabriel Lopez <gywlopez9@gmail.com>
2024-09-18 22:05:43 -0600

    Menu de opciones para usuarios listo

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener } from '@angular/core';
+import { AppLayoutComponent } from './app.layout.component';
+import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { LayoutService } from "./service/app.layout.service";
 
@@ -16,5 +18,63 @@ export class AppTopBarComponent {
 
     @ViewChild('topbarmenu') menu!: ElementRef;
 
-    constructor(public layoutService: LayoutService) { }
+    opcionesDesplegadas: boolean = false;
+    constructor(
+        public layoutService: LayoutService,
+        private router: Router,
+        private appLayoutComponent: AppLayoutComponent,
+        private ElementRef: ElementRef,
+    ) { }
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Perfil',
+                icon: 'pi pi-user',
+                items: [
+                    {
+                        label: 'Editar',
+                        icon: 'pi pi-user-edit',
+                        command: () => this.editPerfil()
+                    },
+                    {
+
+                        label: 'Contraseña',
+                        icon: 'pi pi-key',
+                        command: () => this.editContrasena()
+                    },
+                    {
+
+                        label: 'Salir',
+                        icon: 'pi pi-sign-in',
+                        command: () => this.salir()
+                    },
+                ]
+            },
+
+        ];
+
+    }
+
+
+    salir() {
+        
+        this.ocultarOpciones();
+    }
+
+    editPerfil(){
+        this.router.navigate(['/perfiles/perfil']);
+        this.ocultarOpciones();
+    }
+    editContrasena(){
+        this.router.navigate(['/perfiles/contra']);
+        this.ocultarOpciones();
+    }
+    toggleOpciones() {
+        this.opcionesDesplegadas = !this.opcionesDesplegadas;
+    }
+
+    ocultarOpciones() {
+        this.opcionesDesplegadas = false;
+    }
 }
